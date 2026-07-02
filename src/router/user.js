9a6fdd3e568b6c3ae5a2f83e13d7cc7f9122428b
Ahmadzaki <ahmadzakinaibi57@gmail.com>
2026-07-02 17:33:44 +0430

    authenticate users endpoint 2

--- src/router/user.js
+++ src/router/user.js
@@ -62,36 +62,7 @@ router.get("/users/me", auth, async (req, res) => {
   res.status(200).send(req.user);
 });
 
-// router.get("/users/me",auth, async (req, res) => {
-//   res.status(200).send(req.user);
-//   // try {
-//   //   const users = await User.find({});
-//   //   res.status(200).send(users);
-//   // } catch (e) {
-//   //   res.status(500).send({
-//   //     error: e.message,
-//   //   });
-//   // }
-// });
-
-router.get("/users/:id", async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      return res.status(404).send({
-        error: "User not found",
-      });
-    }
-
-    res.status(200).send(user);
-  } catch (e) {
-    res.status(400).send({
-      error: "Invalid user ID",
-    });
-  }
-});
-
-router.patch("/users/:id", async (req, res) => {
+router.patch("/users/me", auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ["name", "email", "password"];
   const isValidOperation = updates.every((update) =>
@@ -104,17 +75,13 @@ router.patch("/users/:id", async (req, res) => {
     });
   }
   try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      return res.status(404).send({
-        error: "User not found",
-      });
-    }
+   
+    
     updates.forEach((update) => {
-      user[update] = req.body[update];
+      req.user[update] = req.body[update];
     });
-    await user.save();
-    res.status(200).send(user);
+    await req.user.save();
+    res.status(200).send(req.user);
   } catch (e) {
     res.status(500).send({
       error: e.message,
@@ -122,18 +89,93 @@ router.patch("/users/:id", async (req, res) => {
   }
 });
 
-router.delete("/users/:id", async (req, res) => {
+router.delete("/users/me", auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) {
-      return res.status(404).send({
-        error: "User not found",
-      });
-    }
-    res.status(200).send(user);
+    // Use deleteOne() instead of remove()
+    await req.user.deleteOne();
+    res.status(200).send(req.user);
   } catch (e) {
     res.status(500).send({
-      error: e.message,
-    });
+      error: e.message
+    })
   }
-});
+})
+
+
+
+// router.get("/users/me",auth, async (req, res) => {
+//   res.status(200).send(req.user);
+//   // try {
+//   //   const users = await User.find({});
+//   //   res.status(200).send(users);
+//   // } catch (e) {
+//   //   res.status(500).send({
+//   //     error: e.message,
+//   //   });
+//   // }
+// });
+
+// router.get("/users/:id", async (req, res) => {
+//   try {
+//     const user = await User.findById(req.params.id);
+//     if (!user) {
+//       return res.status(404).send({
+//         error: "User not found",
+//       });
+//     }
+
+//     res.status(200).send(user);
+//   } catch (e) {
+//     res.status(400).send({
+//       error: "Invalid user ID",
+//     });
+//   }
+// });
+
+// router.patch("/users/:id", async (req, res) => {
+//   const updates = Object.keys(req.body);
+//   const allowedUpdates = ["name", "email", "password"];
+//   const isValidOperation = updates.every((update) =>
+//     allowedUpdates.includes(update),
+//   );
+
+//   if (!isValidOperation) {
+//     return res.status(400).send({
+//       error: "Invalid updates",
+//     });
+//   }
+//   try {
+//     const user = await User.findById(req.params.id);
+//     if (!user) {
+//       return res.status(404).send({
+//         error: "User not found",
+//       });
+//     }
+//     updates.forEach((update) => {
+//       user[update] = req.body[update];
+//     });
+//     await user.save();
+//     res.status(200).send(user);
+//   } catch (e) {
+//     res.status(500).send({
+//       error: e.message,
+//     });
+//   }
+// });
+
+
+// router.delete("/users/:id", async (req, res) => {
+//   try {
+//     const user = await User.findByIdAndDelete(req.params.id);
+//     if (!user) {
+//       return res.status(404).send({
+//         error: "User not found",
+//       });
+//     }
+//     res.status(200).send(user);
+//   } catch (e) {
+//     res.status(500).send({
+//       error: e.message,
+//     });
+//   }
+// });
